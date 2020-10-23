1d3ff77609b7908e20b274c947074fe5ed51f27e
Max Sierra <fr3shkid16@gmail.com>
2020-10-23 10:31:24 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Linq;
+
+namespace helloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string playAgain;
+            ColorPrint(ConsoleColor.DarkCyan, "---> Rock Paper Scissors Game <---");
+
+            while (true)
+            {
+                GameLoop();
+
+                Console.WriteLine("Play again? [Y/N]");
+                string playAgain = Console.ReadLine().ToUpper();
+
+                if (playAgain == "Y")
+                {
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+        }
+
+        static void Greet()
+        {
+            Console.WriteLine("What's your name?");
+            string name = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"Hello {name}");
+            Console.ResetColor();
+        }
+
+        static void ColorPrint(ConsoleColor color, string text)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+
+        static Random rnd = new Random();
+
+        static void GameLoop()
+        {
+            int[] validChoice = { 0, 1, 2 };
+            int player;
+            Console.WriteLine("Enter corresponding number:");
+            Console.WriteLine("(0)✊ (1)👋 (2)🤞");
+
+            while (true)
+            {
+
+                player = Int32.Parse(Console.ReadLine());
+
+                if (validChoice.Contains(player))
+                {
+                    break;
+                }
+                else
+                {
+                    ColorPrint(ConsoleColor.Red, "Please enter one valid option!");
+                }
+            }
+
+            CompareOptions(player);
+        }
+
+        static void CompareOptions(int player)
+        {
+            string[] options = { "Rock", "Paper", "Scissors" };
+            int random = rnd.Next(options.Length);
+            string machine = options[random];
+            string selectionsMessage = $"Machine: {machine} - You: {options[player]}";
+
+            if (machine == "Rock")
+            {
+                if (options[player] == "Rock")
+                {
+                    Console.WriteLine(selectionsMessage);
+                    ColorPrint(ConsoleColor.Yellow, "It's a tied 🎀");
+                }
+                else if (options[player] == "Paper")
+                {
+                    Console.WriteLine(selectionsMessage);
+                    ColorPrint(ConsoleColor.DarkGreen, "You win 🎉!");
+                }
+                else
+                {
+                    Console.WriteLine(selectionsMessage);
+                    ColorPrint(ConsoleColor.DarkGray, "Sorry you lost 😢!");
+                }
+            }
+            else if (machine == "Paper")
+            {
+                if (options[player] == "Paper")
+                {
+                    Console.WriteLine(selectionsMessage);
+                    ColorPrint(ConsoleColor.Yellow, "It's a tied 🎀");
+                }
+                else if (options[player] == "Scissors")
+                {
+                    Console.WriteLine(selectionsMessage);
+                    ColorPrint(ConsoleColor.DarkGreen, "You win 🎉!");
+                }
+                else
+                {
+                    Console.WriteLine(selectionsMessage);
+                    ColorPrint(ConsoleColor.DarkGray, "Sorry you lost 😢!");
+                }
+            }
+            else if (machine == "Scissors")
+            {
+                if (options[player] == "Scissors")
+                {
+                    Console.WriteLine(selectionsMessage);
+                    ColorPrint(ConsoleColor.Yellow, "It's a tied 🎀");
+                }
+                else if (options[player] == "Rock")
+                {
+                    Console.WriteLine(selectionsMessage);
+                    ColorPrint(ConsoleColor.DarkGreen, "You win 🎉!");
+                }
+                else
+                {
+                    Console.WriteLine(selectionsMessage);
+                    ColorPrint(ConsoleColor.DarkGray, "Sorry you lost 😢!");
+                }
+            }
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# ---> Rock Paper Scissors <---
+
+Mini console game built in C#.
+
+You need to have the dotnet framework install to run this program. You can find more info online.
+
+## Start
+
+Run `dotnet run`
+
+## Build
+
+To build an executable you can run `dotnet build`
